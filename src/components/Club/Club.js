8f21fa6c3a07e056d6def88d4a4f26ad3254420c
Andrii-dan <89802969+Andrii-dan@users.noreply.github.com>
2022-09-27 20:14:49 +0200

    club info and stadium photo

--- src/components/Club/Club.js
+++ src/components/Club/Club.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 import Loading from '../StructureComponents/Loading/Loading';
 import ClubSquad from './ClubSquad/ClubSquad';
 import ClubStat from './ClubStat/ClubStat';
-import './Club.scss';
 import WinPercentage from './WinPercentage/WinPercentage';
 import ClubStanding from './ClubStanding/ClubStanding';
+import './Club.scss';
 
 const Club = ({ clubId, clubsList }) => {
 	const [clubInfo, setClubInfo] = useState(false);
@@ -23,13 +23,30 @@ const Club = ({ clubId, clubsList }) => {
 		</div>
 	) : (
 		<>
-			<div className='col-12 club__page'>
-				<img
-					className='club__page-logo'
-					src={clubInfo.logo}
-					alt={`${clubInfo.name} logo`}
-				/>{' '}
-				<h1 className='club__page-title'>{clubInfo.name}</h1>
+			<div
+				className='col-12 club__page'
+				style={{
+					backgroundImage: `url(${clubInfo.stadiumPhoto})`,
+					backgroundPosition: 'center',
+					backgroundSize: 'cover',
+				}}
+			>
+				<div className='club__page-info-container'>
+					<img
+						className='club__page-logo'
+						src={clubInfo.logo}
+						alt={`${clubInfo.name} logo`}
+					/>{' '}
+					<h1 className='club__page-title'>{clubInfo.name}</h1>
+					<span>
+						Coach:<span className='coach'> {clubInfo.coach}</span>
+					</span>
+					<span>Stadium: {clubInfo.stadium}</span>
+					<span>
+						Official Website:
+						<a href={`https://${clubInfo.website}`}> {clubInfo.website}</a>
+					</span>
+				</div>
 			</div>
 			<div className='col-3'>
 				<ClubSquad
